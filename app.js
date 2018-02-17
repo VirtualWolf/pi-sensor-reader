b6c3b0aafde43ad4ac60cfe111f9617baa9bd8fb
VirtualWolf <virtualwolf@virtualwolf.org>
2018-02-17 20:32:16 +1100

    Added Express.js to listen on port 3000 and return the sensor data as JSON.

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const sensorLib = require('node-dht-sensor');
 const request = require('superagent');
+const express = require('express');
 const config = require('./config.json');
+
+const app = express();
+
+const sensorData = {
+    outdoor: {
+        temperature: null,
+        humidity: null,
+        timestamp: null,
+    },
+    indoor: {
+        temperature: null,
+        humidity: null,
+        timestamp: null,
+    },
+};
+
+app.get('/rest/:sensor', (req, res) => {
+    return res.json({
+        temperature: sensorData[req.params.sensor].temperature.toFixed(1),
+        humidity: Math.floor(parseInt(sensorData[req.params.sensor].humidity)),
+        timestamp: sensorData[req.params.sensor].timestamp,
+    });
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
 
 process.on('unhandledRejection', (err) => {
     console.log(err.message, err.stack);
@@ -11,11 +37,17 @@ const sensor = {
         config.sensors.forEach(async sensor => {
             const readout = sensorLib.read(sensor.type, sensor.pin);
 
+            const now = new Date().getTime();
+
+            sensorData[sensor.name].temperature = readout.temperature;
+            sensorData[sensor.name].humidity = readout.humidity;
+            sensorData[sensor.name].timestamp = now;
+
             const payload = {
                 sensor_name: sensor.name,
-                temperature: readout.temperature,
-                humidity: readout.humidity,
-                timestamp: new Date().getTime(),
+                temperature: sensorData[sensor.name].temperature,
+                humidity: sensorData[sensor.name].humidity,
+                timestamp: sensorData[sensor.name].timestamp,
             };
 
             await request
