d2f63ddabc91bbda15a8815627bdfde743161930
VirtualWolf <virtualwolf@virtualwolf.org>
2019-08-17 13:02:38 +1000

    Added check to compare the latest readout from the sensor to the previous one stored, to avoid spurious jumps in temperature readings.

--- src/lib/sensor.ts
+++ src/lib/sensor.ts
@@ -34,11 +34,34 @@ export class Sensor {
         try {
             const readout = await sensorLib.read(this.type, this.pin);
 
-            this.temperature = readout.temperature;
-            this.humidity = readout.humidity;
-            this.timestamp = new Date().getTime();
+            // We only want to check the temperature difference between the
+            // latest reading from the sensor and what this.temperature is
+            // if this is _not_ the first time the sensor is taking a reading
+            // (i.e. the very first reading after application start).
+            if (this.temperature !== 0 && this.humidity !== 0) {
+                // Check the absolute difference between the two values, and if they
+                // differ by more than 0.3 degrees, don't update this.temperature.
+                // This helps guard against both the sensor giving weird spurious
+                // temperatures, and also inadvertent direct sunlight on the sensor
+                // during certain times of day!
+                const difference = Math.abs(this.temperature - readout.temperature);
+
+                if (difference <= 0.3) {
+                    this.temperature = readout.temperature;
+                    this.humidity = readout.humidity;
+                    this.timestamp = new Date().getTime();
+                } else {
+                    log(`[${this.name}] Skipping because difference is too large. Currently ${this.temperature}, got ${readout.temperature}`);
+                    return;
+                }
+            } else {
+                log(`[${this.name}] First run of application, setting first reading`)
+                this.temperature = readout.temperature;
+                this.humidity = readout.humidity;
+                this.timestamp = new Date().getTime();
+            }
         } catch (err) {
-            log('Failed to read sensor data: ' + err);
+            log(`[${this.name}] Failed to read sensor data: ${err}`);
         }
     }
 
